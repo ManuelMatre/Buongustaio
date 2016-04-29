83ad03fbf641c230e156bf42cd9f813f453f0f44
Manuel Matrecitos <jmanuel.matrecitos@hotmail.com>
2016-04-29 11:20:47 -0700

    Nuevo proyecto Buongustaio

---
+++ Buongustaio/Buongustaio/Controllers/VentasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Buongustaio.Controllers
+{
+    public class VentasController : Controller
+    {
+        // GET: Ventas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
