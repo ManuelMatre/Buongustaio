5fc94ee0cb74efba54fe91fa21f924f8a4228a0a
Jonathan Benitez <jonaben93@gmail.com>
2016-04-29 12:34:03 -0700

    esto es una prueba

---
+++ Buongustaio/Buongustaio/Controllers/holaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Buongustaio.Controllers
+{
+    public class holaController : Controller
+    {
+        // GET: hola
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
