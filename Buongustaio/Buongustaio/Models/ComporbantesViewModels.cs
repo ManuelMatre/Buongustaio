e47b6fd780ee6a75b812405f93e5668a9a112719
Jonathan Benitez <jonaben93@gmail.com>
2016-05-09 10:55:27 -0700

    banco parte 1

---
+++ Buongustaio/Buongustaio/Models/ComporbantesViewModels.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Buongustaio.Models
+{
+    public class registrarComprobante{
+        [Required]
+        [DataType(DataType.CreditCard)]
+        [Display(Name = "Num de Tarjeta: ")]
+        public int numTarjeta { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Fecha de Expiracion: ")]
+        public int fechaExpriacion { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Nombre de Propietario: ")]
+        public int Nombre { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Clave de Seguridad: ")]
+        public int claveSeguridad { get; set; }
+
+        [Required]
+        [DataType(DataType.CreditCard)]
+        [Display(Name = "Cantidad: ")]
+        public int Cantidad { get; set; }
+    }
+}
