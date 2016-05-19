8641289a799d5def07cf8c754566cf609ba788c4
Manuel Matrecitos <jmanuel.matrecitos@hotmail.com>
2016-05-18 19:45:06 -0700

    Agregados archivos nuevos

---
+++ Buongustaio/Buongustaio/Controllers/EntitiesControllers/PedidosController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Buongustaio.Models;
+using Buongustaio.Classes;
+
+namespace Buongustaio.Controllers.EntitiesControllers
+{
+    public class PedidosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Pedidos
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Pedidos.ToListAsync());
+        }
+
+        // GET: Pedidos/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = await db.Pedidos.FindAsync(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pedidos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Cliente,Pedido,Fecha")] Ordenes ordenes, float total)
+        {
+            Pedidos pedidos = new Pedidos();
+            pedidos.Id = IdUnico.GetUniqueKey();
+            pedidos.Fecha = DateTime.Now;
+            pedidos.Cliente = ordenes.Cliente;
+            pedidos.Pedido = ordenes.Pedido;
+            pedidos.PagoTotal = total;
+            if (ModelState.IsValid)
+            {
+                db.Pedidos.Add(pedidos);
+                await db.SaveChangesAsync();
+                return RedirectToAction("../Home");
+            }
+
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = await db.Pedidos.FindAsync(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedidos);
+        }
+
+        // POST: Pedidos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Cliente,Pedido,Fecha")] Pedidos pedidos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pedidos).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = await db.Pedidos.FindAsync(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedidos);
+        }
+
+        // POST: Pedidos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Pedidos pedidos = await db.Pedidos.FindAsync(id);
+            db.Pedidos.Remove(pedidos);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
