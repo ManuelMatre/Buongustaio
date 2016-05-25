1771cc4adc5957e1b585500137fd7802bd505674
Manuel Matrecitos <jmanuel.matrecitos@hotmail.com>
2016-05-25 15:07:51 -0700

    Conexion con banco

---
+++ Buongustaio/Buongustaio/Classes/MiPago.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Buongustaio.Classes
+{
+    public class MiPago
+    {
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+        public string Token { get; set; }
+        public double Amount { get; set; }
+    }
+}
